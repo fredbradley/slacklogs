2ad7946f3a7f1da57bad657ac387dd47f4f45cc0
Fred Bradley <hello@fredbradley.co.uk>
2017-04-06 13:23:44 +0100

    Put ifstatement in so that the vendor publish action still works despite the lack of the config

--- src/SlackServiceProvider.php
+++ src/SlackServiceProvider.php
@@ -17,43 +17,44 @@ class SlackServiceProvider extends \Illuminate\Support\ServiceProvider
 	    $this->publishes([
 			__DIR__.'/config.php' => config_path('slacklogs.php')
 		], 'slackconfig');
-		
-		if (Config::has('slacklogs.webhook') && config('slacklogs.webhook')!=null)
-		{
-			Log::listen(function()
+		if (file_exists(config_path('slacklogs.php'))) {
+			if (Config::has('slacklogs.webhook'))
 			{
-				$monolog = Log::getMonolog();
-	
-				if (env('APP_ENV') === 'local')
+				Log::listen(function()
 				{
-					$monolog->pushHandler($chromeHandler = new Monolog\Handler\ChromePHPHandler());
-					$chromeHandler->setFormatter(new Monolog\Formatter\ChromePHPFormatter());
-				}
-				
-				if (env('APP_ENV')==='local') {
-					$slack_channel = config('slacklogs.channel_dev');
-				} else {
-					$slack_channel = config('slacklogs.channel_master');
-				}
-				
-				$slackHandler = new Monolog\Handler\SlackWebhookHandler(
-					config('slacklogs.webhook'),
-					$slack_channel,
-					config('slacklogs.name'),
-					true,
-					":eyes:",
-					true,
-					false,
-					Monolog\Logger::DEBUG
-				);
-	            
-				$monolog->pushHandler($slackHandler);
-				$slackHandler->setFormatter(new Monolog\Formatter\LineFormatter());
-			});
-		} else {
-			$errmsg = "It looks like you're trying to use the SlackLogs Webhook, but you haven't defined a webhook in `config/slacklogs.php`";
-			throw new \Exception($errmsg);
-			Log::error($errmsg);
+					$monolog = Log::getMonolog();
+		
+					if (env('APP_ENV') === 'local')
+					{
+						$monolog->pushHandler($chromeHandler = new Monolog\Handler\ChromePHPHandler());
+						$chromeHandler->setFormatter(new Monolog\Formatter\ChromePHPFormatter());
+					}
+					
+					if (env('APP_ENV')==='local') {
+						$slack_channel = config('slacklogs.channel_dev');
+					} else {
+						$slack_channel = config('slacklogs.channel_master');
+					}
+					
+					$slackHandler = new Monolog\Handler\SlackWebhookHandler(
+						config('slacklogs.webhook'),
+						$slack_channel,
+						config('slacklogs.name'),
+						true,
+						":eyes:",
+						true,
+						false,
+						Monolog\Logger::DEBUG
+					);
+		            
+					$monolog->pushHandler($slackHandler);
+					$slackHandler->setFormatter(new Monolog\Formatter\LineFormatter());
+				});
+			} else {
+				$errmsg = "It looks like you're trying to use the SlackLogs Webhook, but you haven't defined a webhook in `config/slacklogs.php`";
+				throw new \Exception($errmsg);
+				Log::error($errmsg);
+			}
 		}
     }
 
